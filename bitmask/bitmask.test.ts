3231194e9e641b1bfc5a9908118a9e5b5cd46f16
JohnWebb4 <John.Webb94@gmail.com>
2018-07-27 12:20:58 -0700

    Test bitmask check bit checks bit

--- bitmask/bitmask.test.ts
+++ bitmask/bitmask.test.ts
@@ -1,26 +1,32 @@
-import { checkBit, setBit } from './bitmask';
+import { checkBit, setBit } from "./bitmask";
 
-describe('bitmask', () => {
-  describe('check bit', () => {
-    it('should be a function', () => {
+describe("bitmask", () => {
+  describe("check bit", () => {
+    it("should be a function", () => {
       expect(checkBit).toBeInstanceOf(Function);
     });
 
-    it('should return a number', () => {
-      expect(typeof checkBit(0, 0)).toBe('number');
+    it("should return a number", () => {
+      expect(typeof checkBit(0, 0)).toBe("number");
+    });
+
+    it("should return checked bit", () => {
+      expect(checkBit(0, 0)).toEqual(0);
+      expect(checkBit(1, 1)).toEqual(0);
+      expect(checkBit(2, 1)).toEqual(2);
     });
   });
 
-  describe('set bit', () => {
-    it('should be a function', () => {
-      expect(setBit).toBeInstanceOf(Function); 
+  describe("set bit", () => {
+    it("should be a function", () => {
+      expect(setBit).toBeInstanceOf(Function);
     });
 
-    it('should return number', () => {
-      expect(typeof setBit(1, 0)).toBe('number');
+    it("should return number", () => {
+      expect(typeof setBit(1, 0)).toBe("number");
     });
 
-    it('should set bit', () => {
+    it("should set bit", () => {
       expect(setBit(1, 0)).toEqual(1);
       expect(setBit(1, 1)).toEqual(3);
     });
