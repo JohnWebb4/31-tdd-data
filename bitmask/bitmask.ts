7cceda30fd45f14218bb4b247dfd0ba16cfdcf51
JohnWebb4 <John.Webb94@gmail.com>
2018-07-27 12:22:12 -0700

    Check and set bits

--- bitmask/bitmask.ts
+++ bitmask/bitmask.ts
@@ -8,10 +8,25 @@ const setBit = (bits: number, bitIndex: number): number => {
   // Create filter
   const filter = 1 << bitIndex;
 
+  // Filter and return
   return bits | filter;
 };
 
+/**
+ * Check a bit is set
+ * @param bits Bit array to check
+ * @param bitIndex Bit index to check
+ * @return Value if bit is set
+ */
+const checkBit = (bits: number, bitIndex: number): number => {
+  // Create filter
+  const filter = 1 << bitIndex;
+
+  // Filter and return
+  return bits & filter;
+};
 
 export {
+  checkBit,
   setBit,
-}
+};
