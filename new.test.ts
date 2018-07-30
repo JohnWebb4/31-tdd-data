460ba6449b6fc5c5ffa173ac301d5ef554b148b0
JohnWebb4 <John.Webb94@gmail.com>
2018-07-30 12:32:04 -0700

    Update all and new tests for linked list section

--- new.test.ts
+++ new.test.ts
@@ -1,1 +1,4 @@
-import './bitmask/bitmask.test';
+import "./linkedlist/linkedlist.test";
+import "./linkedlist/queue.test";
+import "./linkedlist/singlevertex.test";
+import "./linkedlist/stack.test";
