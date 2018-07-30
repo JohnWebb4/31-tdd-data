2630a28533eb10cb266cc47688b12e50af2969b6
JohnWebb4 <John.Webb94@gmail.com>
2018-07-30 14:42:50 -0700

    Complete linked list

--- linkedlist/linkedlist.ts
+++ linkedlist/linkedlist.ts
@@ -40,7 +40,28 @@ class LinkedList {
    * @returns Value at index
    */
   public get(index: number): any {
-    return 0;
+    if (index < 0) {
+      throw new Error("Index must be greater than or equal to zero");
+    }
+
+    // Declare current node
+    let node = this.head;
+
+    // Create index counter
+    let indexCount = 0;
+
+    try {
+      while (indexCount < index) {
+        node = node.next;
+        indexCount += 1;
+      }
+
+      return node.value;
+    } catch (e) {
+      // Could not read to index,
+      // value return undefined
+      return undefined;
+    }
   }
 
   /**
@@ -50,16 +71,123 @@ class LinkedList {
    * @returns Value inserted
    */
   public insert(value: any, index?: number): any {
-    return 0;
+    // Create vertex
+    const vertex = new SingleNode(value);
+
+    if (this.head === null && this.tail === null) {
+      // If empty list, insert into list
+      this.head = vertex;
+      this.tail = vertex;
+
+      // Return value
+      return value;
+    }
+
+    if (index === undefined) {
+      // If no index, add to tail
+      this.tail.next = vertex;
+      this.tail = vertex;
+
+      // Return value
+      return value;
+    }
+
+    if (index < 0) {
+      // Invalid index, throw error
+      throw new Error("Index must be greater than or equal to zero");
+    }
+
+    if (index === 0) {
+      // If inserting at head, insert
+      vertex.next = this.head;
+      this.head = vertex;
+
+      // Return value
+      return value;
+    }
+
+    // Else loop to index
+    let node = this.head;
+    let posCount = 0;
+
+    while (posCount < index - 1 && node.next !== null) {
+      // Loop through linked list till at index or at end of list
+      node = node.next;
+      posCount += 1;
+    }
+
+    // Add to list
+    const nextNode = node.next;
+    node.next = vertex;
+    vertex.next = nextNode;
+
+    // Return value
+    return value;
   }
 
   /**
    * Remove value from list
    * @param index Index to remove from
+   * @throws if index if out of range
    * @returns Value removed
    */
   public remove(index: number): any {
-    return 0;
+    if (index < 0) {
+      // if invalid index, throw error
+      throw new Error("Index must be greater than or equal to zero");
+    }
+
+    // Declare previous node
+    let prevNode = null;
+
+    // Declare current node
+    let node = this.head;
+
+    // Declare index count
+    let indexCount = 0;
+
+    try {
+      while (indexCount < index) {
+        // Update node and previous node
+        prevNode = node;
+        node = node.next;
+
+        // Update index count
+        indexCount += 1;
+      }
+
+      if (node === this.head && node === this.tail) {
+          // If head is tail, this is on element list
+          // make empty list
+          this.head = null;
+          this.tail = null;
+
+          // Return value
+          return node.value;
+      } else if (node === this.head) {
+        // If head, move head forward
+        // Declare previous head
+        const prevHead = this.head;
+
+        // Update head
+        this.head = this.head.next;
+
+        // Return value of head
+        return prevHead.value;
+      } else if (node === this.tail) {
+        // If at tail, go to previous node
+        this.tail = prevNode;
+      }
+
+      // Remove node
+      prevNode.next = node.next;
+
+      // Return node value
+      return node.value;
+    } catch (e) {
+      // Index is out of range, throw error
+      throw new Error("Index is out of range");
+    }
   }
 
   /**
@@ -69,7 +197,23 @@ class LinkedList {
    * @returns Index of first occurrence in list or -1 if not in list
    */
   public search(value: any): number {
-    return 0;
+    let node = this.head;
+    let index = 0;
+
+    try {
+      while (node.value !== value) {
+        node = node.next;
+        index += 1;
+      }
+
+      // Found value in list,
+      // return index
+      return index;
+    } catch (e) {
+      // Could not find value in list
+      // Return -1
+      return -1;
+    }
   }
 }
 
--- new.test.ts
+++ new.test.ts
@@ -1,1 +1,5 @@
 // Add all test to be run with "npm run test:new" here
+import "./linkedlist/linkedlist.test";
+import "./linkedlist/queue.test";
+import "./linkedlist/singlenode.test";
+import "./linkedlist/stack.test";
