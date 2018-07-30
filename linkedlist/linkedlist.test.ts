9ff76b200ac165160d50b061450cde91ffb1134c
JohnWebb4 <John.Webb94@gmail.com>
2018-07-30 12:06:50 -0700

    Test linked list has, get, insert, remove, and search

--- linkedlist/linkedlist.test.ts
+++ linkedlist/linkedlist.test.ts
@@ -4,4 +4,11 @@ describe("Linked List", () => {
   test("is a class", () => {
     expect(LinkedList).toBeInstanceOf(Function);
   });
+
+  test("has get, insert, remove and search", () => {
+    expect(LinkedList.prototype.get).toBeInstanceOf(Function);
+    expect(LinkedList.prototype.insert).toBeInstanceOf(Function);
+    expect(LinkedList.prototype.remove).toBeInstanceOf(Function);
+    expect(LinkedList.prototype.search).toBeInstanceOf(Function);
+  });
 });
