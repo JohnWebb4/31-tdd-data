2b6fd9311619b9fcb3deaab9536623c3e1020946
JohnWebb4 <John.Webb94@gmail.com>
2018-07-30 13:21:37 -0700

    Test can get values at index and insert any data type

--- linkedlist/linkedlist.test.ts
+++ linkedlist/linkedlist.test.ts
@@ -65,5 +65,49 @@ describe("Linked List", () => {
 
     expect(list.head.value).toBe(-1);
     expect(list.head.next.value).toBe(0);
+
+    // Create new list
+    const newList = new LinkedList();
+
+    // Add different data types
+    newList.insert(0);
+    newList.insert("String");
+    newList.insert([1]);
+    newList.insert(true);
+    newList.insert({ test: true});
+
+    // Test inserted different data types
+    expect(newList.head.value).toBe(0);
+    expect(newList.head.next.value).toBe("String");
+    expect(newList.head.next.next.value).toEqual([1]);
+    expect(newList.head.next.next.next.value).toBe(true);
+    expect(newList.head.next.next.next.next.value).toEqual({ test: true });
+  });
+
+  test("can get value at index", () => {
+    // Create linked list
+    const list = new LinkedList();
+
+    // Test getting value in empty list
+    expect(list.get(0)).toBe(undefined);
+
+    // Insert value
+    list.insert(0);
+
+    // Test can get value
+    expect(list.get(0)).toBe(0);
+
+    // Test can't get value out of range
+    expect(list.get(1)).toBe(undefined);
+
+    // Insert some more
+    list.insert(1);
+    list.insert("String");
+    list.insert([]);
+
+    // Test can get values
+    expect(list.get(1)).toBe(1);
+    expect(list.get(2)).toBe("String");
+    expect(list.get(3)).toEqual([]);
   });
 });
