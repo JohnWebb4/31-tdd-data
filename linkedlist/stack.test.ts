4c2e2222cc7dc5f8498401e9d39c8af662aa1496
JohnWebb4 <John.Webb94@gmail.com>
2018-07-30 12:31:15 -0700

    Test creates a stack

--- linkedlist/stack.test.ts
+++ linkedlist/stack.test.ts
@@ -9,4 +9,21 @@ describe("Stack", () => {
     expect(Stack.prototype.pop).toBeInstanceOf(Function);
     expect(Stack.prototype.push).toBeInstanceOf(Function);
   });
+
+  test("creates a stack", () => {
+    // Create with initial value
+    const stack = new Stack(0);
+
+    // Test stack
+    expect(stack.head.value).toBe(0);
+    expect(stack.head.next).toBe(null);
+    expect(stack.tail).toBe(stack.head);
+
+    // Create empty stack
+    const emptyStack = new Stack();
+
+    // Test empty stack
+    expect(emptyStack.head).toBe(null);
+    expect(emptyStack.tail).toBe(null);
+  });
 });
