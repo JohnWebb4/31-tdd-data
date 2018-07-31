03671fe4a9c8999c9e700e2eb4eac8c4f0fdef07
JohnWebb4 <John.Webb94@gmail.com>
2018-07-30 19:58:54 -0700

    Build using build.ts

---
+++ build.ts
@@ -0,0 +1,10 @@
+// Import all structures to export
+// Export by running npm run build
+// or a derived command there of (build:browser, etc)
+
+import "./bitmask/bitmask";
+
+import "./linkedlist/linkedlist";
+import "./linkedlist/queue";
+import "./linkedlist/singlenode";
+import "./linkedlist/stack";
