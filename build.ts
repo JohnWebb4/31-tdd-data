1411e0d748ef66eddcaa6c3f8abcb61966dee627
JohnWebb4 <John.Webb94@gmail.com>
2018-07-31 15:20:20 -0700

    Setup building for browser global scope

--- build.ts
+++ build.ts
@@ -1,10 +1,33 @@
-// Import all structures to export
-// Export by running npm run build
+/* tslint:disable interface-name*/
+
+// Import all structures to export here
+// Run npm build to compile
 // or a derived command there of (build:browser, etc)
 
-import "./bitmask/bitmask";
+import * as Bitmask from "./build/bitmask/bitmask";
 
-import "./linkedlist/linkedlist";
-import "./linkedlist/queue";
-import "./linkedlist/singlenode";
-import "./linkedlist/stack";
+import { LinkedList } from "./linkedlist/linkedlist";
+import { Queue } from "./linkedlist/queue";
+import { SingleNode } from "./linkedlist/singlenode";
+import { Stack } from "./linkedlist/stack";
+
+// This is set up for commonJS imports (Node style).
+// To enable browser imports you can include the commonJS
+// library, or you can declare in the global (window) scope
+// by following the instructions below
+
+// If you wish for the export to be available in the global
+// scope, then you must declare Window interface with property
+// name of type any. You must then assign the export to
+// window.[name].
+// See the example below.
+
+// declare global {
+//   interface Window {
+//     bitmask: any;
+//     LinkedList: any;
+//   }
+// }
+
+// window.bitmask = bitmask;
+// window.LinkedList = LinkedList;
