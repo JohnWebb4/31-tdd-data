e66ea17b5cfac3333dfd51b5669def282a7fa826
JohnWebb4 <John.Webb94@gmail.com>
2018-07-30 18:45:33 -0700

    Test pop values from stac

--- linkedlist/stack.test.ts
+++ linkedlist/stack.test.ts
@@ -28,15 +28,53 @@ describe("Stack", () => {
   });
 
   test("can push to stack", () => {
+    // Create stack
     const stack = new Stack();
 
+    // Push value
     expect(stack.push(1)).toBe(1);
     expect(stack.tail.value).toBe(1);
 
+    // Push value
     expect(stack.push(2)).toBe(2);
     expect(stack.tail.value).toBe(2);
 
+    // Push value
     expect(stack.push(3)).toBe(3);
     expect(stack.tail.value).toBe(3);
+  });
+
+  test("can pop from stack", () => {
+    // Create stack
+    const stack = new Stack();
+
+    // Push value
+    stack.push(1);
+
+    // Pop value
+    expect(stack.pop()).toBe(1);
+    expect(stack.head).toBe(null);
+    expect(stack.tail).toBe(null);
+
+    // Push value
+    stack.push(2);
+    expect(stack.head.value).toBe(2);
+
+    // Push value
+    stack.push(3);
+    expect(stack.tail.value).toBe(3);
+
+    // Push value
+    stack.push(4);
+    expect(stack.tail.value).toBe(4);
+
+    // Pop all values
+    expect(stack.pop()).toBe(4);
+    expect(stack.pop()).toBe(3);
+    expect(stack.pop()).toBe(2);
+
+    // test empty list
+    expect(stack.head).toBe(null);
+    expect(stack.tail).toBe(null);
   });
 });
