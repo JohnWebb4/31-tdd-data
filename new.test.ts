9cea8b020a098d64fcd8f7a93ba24afbbe8bdd8c
JohnWebb4 <John.Webb94@gmail.com>
2018-08-04 13:52:18 -0700

    Change new tests to doubly linked list

--- new.test.ts
+++ new.test.ts
@@ -1,5 +1,3 @@
 // Add all test to be run with "npm run test:new" here
-import "./linkedlist/linkedlist.test";
-import "./linkedlist/queue.test";
-import "./linkedlist/singlenode.test";
-import "./linkedlist/stack.test";
+import "./linkedlist/doublenode.test";
+import "./linkedlist/doublylinkedlist.test";
